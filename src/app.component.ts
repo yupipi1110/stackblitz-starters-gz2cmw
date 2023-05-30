8b6cceb2d2d0a8d9c4d74f0b5b994b7b231bb621
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-30 23:20:56 +0900

    3 files modified

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,34 +1,26 @@
-import 'zone.js/dist/zone';
-import { AppModule } from './app.module';
 import { Component } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AgGridModule } from 'ag-grid-angular';
+import 'ag-grid-enterprise';
 
 @Component({
   selector: 'my-app',
   template: `
-    <ag-grid-angular style="width: 500px; height: 300px;"
-      class="ag-theme-alpine"
-      [rowData]="rowData"
-      [columnDefs]="columnDefs">
+    <ag-grid-angular style="width: 500px; height: 300px;" class="ag-theme-alpine"
+      [rowData]="rowData" [columnDefs]="columnDefs">
     </ag-grid-angular>
-  `,
-  styleUrls: ['./app.component.css'],
+  `
 })
 export class AppComponent {
   columnDefs = [
-    { field: 'name' },
-    { field: 'height' },
-    { field: 'weight' },
-    { field: 'price' },
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
   ];
 
   rowData = [
-    { name: 'yamada', height: 150, weight: 80, price: 1000 },
-    { name: 'tanaka', height: 160, weight: 90, price: 2000 },
-    { name: 'yamamoto', height: 180, weight: 30, price: 5000 },
-    { name: 'ito', height: 10, weight: 80, price: 100 },
+    { make: 'Toyota', model: 'Camry', price: 25000 },
+    { make: 'Honda', model: 'Civic', price: 22000 },
+    { make: 'Ford', model: 'F-150', price: 35000 }
   ];
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
